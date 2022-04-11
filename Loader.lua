4a332bc6aa822e95fb3af030901385cb777893f6
ViVaWareBoi <88981566+ViVaWareBoi@users.noreply.github.com>
2022-04-11 07:26:41 +0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,11 +1,18 @@
 if game.PlaceId == 6284583030 then
+-- 	Pet sim X
     	loadstring(game:HttpGet(("https://raw.githubusercontent.com/ViVaWareBoi/KitchenHub/main/Main/PetSimulatorX.lua"), true))()
 elseif game.PlaceId == 7560156054 then
+-- 	Clicker simulator
 		loadstring(game:HttpGet(("https://raw.githubusercontent.com/ViVaWareBoi/KitchenHub/main/Main/ClickerSimulator.lua"), true))()
 elseif game.PlaceId == 8554378337 then
+-- 	Weapon Fighting Simulator
 		loadstring(game:HttpGet(("https://raw.githubusercontent.com/ViVaWareBoi/KitchenHub/main/Main/WeaponFightingSimulator.lua"), true))()
 elseif game.PlaceId == 171391948 then
+-- 	Vehicle Simulator
 		loadstring(game:HttpGet(("https://raw.githubusercontent.com/ViVaWareBoi/KitchenHub/main/Main/VehicleSimulator.lua"), true))()
+elseif game.PlaceId == 537413528 then
+-- 	Build a boat
+	loadstring(game:HttpGet(("https://raw.githubusercontent.com/ViVaWareBoi/KitchenHub/main/Main/Buildaboat.lua"), true))()
 else
 	local CoreGui = game:GetService("StarterGui")
 
