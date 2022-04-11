45b3896ce54f3cd386ffbd9b859d77a2850c92c6
ViVaWareBoi <88981566+ViVaWareBoi@users.noreply.github.com>
2022-04-11 09:26:35 +0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,31 +1,32 @@
-if game.PlaceId == 6284583030 then
--- 	Pet sim X
-    	loadstring(game:HttpGet(("https://raw.githubusercontent.com/ViVaWareBoi/KitchenHub/main/Main/PetSimulatorX.lua"), true))()
-elseif game.PlaceId == 7560156054 then
--- 	Clicker simulator
-		loadstring(game:HttpGet(("https://raw.githubusercontent.com/ViVaWareBoi/KitchenHub/main/Main/ClickerSimulator.lua"), true))()
-elseif game.PlaceId == 8554378337 then
--- 	Weapon Fighting Simulator
-		loadstring(game:HttpGet(("https://raw.githubusercontent.com/ViVaWareBoi/KitchenHub/main/Main/WeaponFightingSimulator.lua"), true))()
-elseif game.PlaceId == 171391948 then
--- 	Vehicle Simulator
-		loadstring(game:HttpGet(("https://raw.githubusercontent.com/ViVaWareBoi/KitchenHub/main/Main/VehicleSimulator.lua"), true))()
-elseif game.PlaceId == 537413528 then
--- 	Build a boat
-	loadstring(game:HttpGet(("https://raw.githubusercontent.com/ViVaWareBoi/KitchenHub/main/Main/Buildaboat.lua"), true))()
-else
-	local CoreGui = game:GetService("StarterGui")
+-- Credits to sadge#2006 for this sexy loader thing!
 
-	CoreGui:SetCore("SendNotification", {
-		Title = "Game not supported :(";
-		Text = "Copying discord to clipboard... Please check the game support channel for more information.";
-		Duration = 3;
-	})
-	wait(2)
-	setclipboard("https://discord.gg/R3QMj5U8Pn")
-	CoreGui:SetCore("SendNotification", {
-		Title = "Copied Succesfully";
-		Text = "Paste the link in your browser to join the discord, and then check the game support channel for more info";
-		Duration = 3;
-	})
+local CoreGui = game:GetService("StarterGui")
+
+local GameTable = {
+    [6284583030] = "https://raw.githubusercontent.com/ViVaWareBoi/KitchenHub/main/Main/PetSimulatorX.lua",
+    [7560156054] = "https://raw.githubusercontent.com/ViVaWareBoi/KitchenHub/main/Main/ClickerSimulator.lua",
+    [8554378337] = "https://raw.githubusercontent.com/ViVaWareBoi/KitchenHub/main/Main/WeaponFightingSimulator.lua",
+    [171391948] = "https://raw.githubusercontent.com/ViVaWareBoi/KitchenHub/main/Main/VehicleSimulator.lua",
+    [537413528] = "https://raw.githubusercontent.com/ViVaWareBoi/KitchenHub/main/Main/Buildaboat.lua";
+}
+
+
+
+if GameTable[game.PlaceId] == nil then
+    CoreGui:SetCore("SendNotification", {
+        Title = "Game not supported :(";
+        Text = "Copying discord to clipboard... Please check the game support channel for more information.";
+        Duration = 3;
+    })
+    
+    task.delay(2, function()
+        setclipboard("https://discord.gg/R3QMj5U8Pn")
+            CoreGui:SetCore("SendNotification", {
+            Title = "Copied Succesfully";
+            Text = "Paste the link in your browser to join the discord, and then check the game support channel for more info";
+            Duration = 3;
+        })
+    end)
+else
+    loadstring(game:HttpGet(GameTable[game.PlaceId]))()
 end
